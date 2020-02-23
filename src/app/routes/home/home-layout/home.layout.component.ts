d5ed08fcd05fc2798761902d4dfc8cd0aab3a90c
istardev <istardev@outlook.com>
2020-02-23 10:26:11 +0200

    work on performance

--- src/app/routes/home/home-layout/home.layout.component.ts
+++ src/app/routes/home/home-layout/home.layout.component.ts
@@ -1,21 +1,41 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { ShareService } from '@shared/services/share.service';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil, throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-layout',
   templateUrl: './home-layout.component.html',
   styleUrls: ['./home-layout.component.scss'],
 })
-export class HomeLayoutComponent implements OnInit {
+export class HomeLayoutComponent implements OnInit, OnDestroy {
   imageOverlay = false;
   scrollTop = 0;
+  destroy$ = new Subject<boolean>();
 
-  @HostListener('window:scroll', ['$event'])
-  public moveBackground() {
-    this.shareService.setOffsetTop(window.scrollY);
+  constructor(
+    private shareService: ShareService,
+    private ngZone: NgZone
+  ) {}
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.addScrollSubscription();
+    });
   }
 
-  constructor(private shareService: ShareService) {}
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 
-  ngOnInit(): void {}
+  addScrollSubscription() {
+    fromEvent(window, 'scroll')
+      .pipe(takeUntil(this.destroy$), throttleTime(30))
+      .subscribe(() => this.moveBackground());
+  }
+
+  moveBackground() {
+    this.shareService.setOffsetTop(window.scrollY);
+  }
 }
--- src/app/shared/directives/parallax/parallax.directive.ts
+++ src/app/shared/directives/parallax/parallax.directive.ts
@@ -1,20 +1,40 @@
-import { Directive, Input, ElementRef, HostListener } from '@angular/core';
+import { Directive, Input, ElementRef, HostListener, OnInit, NgZone, OnDestroy } from '@angular/core';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil, throttleTime } from 'rxjs/operators';
 
 @Directive({
   selector: '[appParallax]'
 })
-export class ParallaxDirective {
+export class ParallaxDirective implements OnInit, OnDestroy {
+  @Input('ratio') parallaxRatio: number = 1;
+  destroy$ = new Subject<boolean>();
+  initialTop = 0;
 
-  @Input('ratio') parallaxRatio: number = 1
-  initialTop: number = 0
-
-  constructor(private eleRef: ElementRef) {
-    this.initialTop = this.eleRef.nativeElement.getBoundingClientRect().top
+  constructor(
+    private eleRef: ElementRef,
+    private ngZone: NgZone
+  ) {
+    this.initialTop = this.eleRef.nativeElement.getBoundingClientRect().top;
   }
 
-  @HostListener("window:scroll", ["$event"])
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.addScrollSubscription();
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  addScrollSubscription() {
+    fromEvent(window, 'scroll')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((e) => this.onWindowScroll(e));
+  }
   onWindowScroll(event) {
-    this.eleRef.nativeElement.style.top = (this.initialTop - (window.scrollY * this.parallaxRatio)) + 'px'
+    const top = this.initialTop - (window.scrollY * this.parallaxRatio);
+    this.eleRef.nativeElement.style.transform = `translateY(${top}px)`;
   }
-
 }
